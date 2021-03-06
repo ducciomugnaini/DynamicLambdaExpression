59c586875984bfb0743d628107bf465fd11298d5
Duccio Mugnaini <duccio.mugnaini@gmail.com>
2021-03-06 11:56:34 +0100

    init repo

---
+++ Program.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace DynamicLambdaExpression
+{
+    class Program
+    {
+        static async Task<int> Main(string[] args)
+        {
+            //---------------------------------------------------------------------
+            //--------------- Static Lambda Exmpression ---------------------------
+            //---------------------------------------------------------------------
+
+            IEnumerable<TennisPlayer> tennisPlayers = Seed();
+            var staticLambdaResult = tennisPlayers.Where(tp => tp.VictoryAmount > 19400000);
+
+            Print("Static Result", staticLambdaResult);
+
+            //---------------------------------------------------------------------
+            //--------------- Dynamic Lambda Exmpression --------------------------
+            //---------------------------------------------------------------------
+
+            var parameterType = Expression.Parameter(Type.GetType("DynamicLambdaExpression.TennisPlayer"), "tp");
+            var parameterProperty = Expression.Property(parameterType, "VictoryAmount");
+            var constant = Expression.Constant(19400000.0);
+            var expression = Expression.GreaterThan(parameterProperty, constant); // tp.VictoryAmount > 19400000
+
+            var lambda = Expression.Lambda<Func<TennisPlayer, bool>>(expression, parameterType); // tp => tp.VictoryAmount > 19400000
+            var compiledLambda = lambda.Compile();
+            var dynamicLambdaResult = tennisPlayers.Where(compiledLambda);
+
+            Print("Dynamic Result", dynamicLambdaResult);
+
+            //---------------------------------------------------------------------
+            //--------------- Dynamic String Lambda Exmpression -------------------
+            //---------------------------------------------------------------------
+
+            var stringFilter = "tp => tp.VictoryAmount > 19400000";
+            var options = ScriptOptions.Default.AddReferences(typeof(Program).Assembly);
+            
+            Func<TennisPlayer, bool> tennisPlayerFiler = await CSharpScript.EvaluateAsync<Func<TennisPlayer, bool>>(stringFilter, options);
+
+            var dynamicStringLambdaResult = tennisPlayers.Where(tennisPlayerFiler);
+
+            Print("Dynamic String Lambda Result", dynamicStringLambdaResult);
+
+            //---------------------------------------------------------------------
+            //--------------- Dynamic Lambda Exmpression --------------------------
+            //---------------------------------------------------------------------
+            /* A LambdaExpression has two components:
+                - a parameter list—(string x)—represented by the Parameters property
+                - a body—x.StartsWith("a")—represented by the Body property.
+             */
+
+            /*var asd = GetExtensionMethods(typeof(Program).Assembly, typeof(double)).ToList();
+
+            var elementType = Type.GetType("DynamicLambdaExpression.TennisPlayer");
+
+            var parameterType_1 = Expression.Parameter(Type.GetType("DynamicLambdaExpression.TennisPlayer"), "tp");
+            var constant_1 = Expression.Constant(19400000.0);
+            Expression body = Expression.Call(
+                Expression.Property(
+                    parameterType_1,
+                    elementType.GetProperties().FirstOrDefault(p => p.Name.Equals("VictoryAmount"))),
+                    GetExtensionMethods(typeof(Program).Assembly, typeof(double)).ToList()[0],
+                constant_1
+            );
+
+            Expression<Func<TennisPlayer, bool>> expr = Expression.Lambda<Func<TennisPlayer, bool>>(body, parameterType_1);
+            var dynamicLambdaResult_1 = tennisPlayers.AsQueryable().Where(expr);
+
+            Print("Dynamic Result with Body Method", dynamicLambdaResult_1);*/
+
+            return 1;
+        }
+
+        private static List<TennisPlayer> Seed()
+        {
+            return new List<TennisPlayer>
+            {
+                new TennisPlayer()
+                {
+                    Name = "Rafael",
+                    Surname = "Nadal",
+                    Age = 35,
+                    VictoryAmount = 15400000
+                },
+                new TennisPlayer()
+                {
+                    Name = "Roger",
+                    Surname = "Federer",
+                    Age = 37,
+                    VictoryAmount = 37640000
+                }
+            };
+        }
+
+        private static void Print(string title, IEnumerable<TennisPlayer> tennisPlayers)
+        {
+            Console.WriteLine(title);
+            foreach (var tp in tennisPlayers)
+            {
+                Console.WriteLine($"{tp.Name} {tp.Surname} | {tp.VictoryAmount}");
+            }
+        }
+
+        static IEnumerable<MethodInfo> GetExtensionMethods(Assembly assembly,
+        Type extendedType)
+        {
+            var query = from type in assembly.GetTypes()
+                        where type.IsSealed && !type.IsGenericType && !type.IsNested
+                        from method in type.GetMethods(BindingFlags.Static
+                            | BindingFlags.Public | BindingFlags.NonPublic)
+                        where method.IsDefined(typeof(ExtensionAttribute), false)
+                        where method.GetParameters()[0].ParameterType == extendedType
+                        select method;
+            return query;
+        }
+    }
+
+    public class TennisPlayer
+    {
+        public String Name { get; set; }
+        public String Surname { get; set; }
+        public int Age { get; set; }
+        public double VictoryAmount { get; set; }
+    }
+
+    public static class DoubleExtension
+    {
+        public static bool GreaterThan(this double a, double b)
+        {
+            return a > b;
+        }
+    }
+
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,107 @@
 # DynamicLambdaExpression
+
+## A Simple Lambda Expression Query
+
+Just about every application requires some form of data access and query capability. Using the data classes and data set illustrated in Listing 1 and Listing 2 respectively, you can employ the following query to find all people with the first name of John:
+
+``` c#
+var result = _people.Where(person => person.FirstName
+ == "John").ToList();
+```
+Lambda expressions, like is the one illustrated above, are very powerful. There is also some inflexibility, although you can easily vary the value you use to compare to the FirstName property. What if you want to vary the property? That is, as it turns out, a bit more complicated.
+
+In the next section, I’ll convert the query listed above to be dynamic so that it can be built and compiled at run time.
+Creating Your First Dynamic Lambda Expression
+
+The key to understanding how to create dynamic lambda expressions rests with understanding the Expressions Class, which is part of the System.Linq.Expressions Namespace. The first step is to create a parameter expression that allows you to validate and compile against a known type. In this case, the type is in the Person and Address Classes:
+
+``` c#
+var parameterExpression =
+ Expression.Parameter(Type.GetType("ExpressionTreeTests.
+Person"), "person");
+```
+
+Once you have you parameter expression, you can begin to build the additional components. The following code creates an expression constant:
+
+``` c#
+var constant = Expression.Constant("John");
+```
+
+The expression constant has to be compared to some property. The code in the next snippet creates a property expression.
+
+``` c#
+var property = Expression.Property(parameterExpression,
+ "FirstName");
+```
+
+Note that the parameter expression created earlier is used to validate the property. If the property name passed as a string isn’t found for the specified type, an error is thrown.
+
+Next, you need to bring the expression constant and property together. In this case, you wish to test whether or not a property is equal to a string constant. The following code brings those two expressions together:
+
+``` c#
+var expression = Expression.Equal(property, constant);
+```
+
+The expression variable’s string representation is:
+
+``` c#
+{(person.FirstName == "John")}
+```
+
+Once an expression has been created, you can create a lambda expression. The next code snippet accomplishes that task:
+
+``` c#
+var lambda = Expression.Lambda<Func<Person,
+ bool>>(expression, parameterExpression);
+```
+
+In this case, the lambda represents a delegate function that accepts a Person argument and returns a Boolean value to indicate whether the specified Person object meets the query criteria. In this case, the query criterion is whether the FirstName == "John". The following illustrates the string representation of the dynamic lambda expression:
+
+``` c#
+{person => (person.FirstName == "John")}
+```
+
+This is identical to the lambda expression in the simple lambda expression query listed above. The only difference is that instead of embedding the definition in the code, you’ve built it dynamically. The last step before using the dynamic lambda expression is to compile it:
+
+``` c#
+var compiledLambda = lambda.Compile();
+```
+
+Now that the lambda is compiled, you can use it in a query:
+
+``` c#
+var result = _people.Where(compiledLambda).ToList();
+```
+
+That’s it. That’s all there is to building a simple dynamic lambda expression. What if you have compound criteria? Let’s tackle that next. 
+
+## From string to lambda with Roslyn
+
+One of the little known gems of Roslyn is that as part of its Scripting API, it ships with an excellent expression evaluator. And this is what we will be using – through the Roslyn scripting packages from Nuget – to solve our problem in almost literally a single line of code.
+
+Of course if you follow this blog, or my Twitter, or my Github, you probably know that I’m deeply involved in the C# scripting community, so if you have any questions about that area I’m always happy to help.
+
+The Nuget package to reference is called Microsoft.CodeAnalysis.CSharp.Scripting.
+
+Once you have it referenced in the project, you can use the C# scripting engine to evaluate our delegate, represented as a raw string, into a real instance of Func lambda that we require to filter our albums. The scripting APIs of Roslyn exposes a static method CSharpScript.EvaluateAsync which is perfect for our use case. We can pass in a string expression and have it compiled and evaluated on the fly, with the full power of the C# compiler. No manual parsing, no worrying about edge cases.
+
+The usage is shown below.
+
+``` c#
+var discountFilter = "album => album.Quantity > 0";
+var options = ScriptOptions.Default.AddReferences(typeof(Album).Assembly);
+
+Func<Album, bool> discountFilterExpression = await CSharpScript.EvaluateAsync<Func<Album, bool>>(discountFilter, options);
+
+var discountedAlbums = albums.Where(discountFilterExpression);
+//hooray now we have discountedAlbums!
+```
+
+# References
+
+[MS Documentation](https://docs.microsoft.com/it-it/dotnet/csharp/programming-guide/concepts/expression-trees/how-to-use-expression-trees-to-build-dynamic-queries)
+
+[Simple Dyamic Expression](https://www.codemag.com/article/1607041/Simplest-Thing-Possible-Dynamic-Lambda-Expressions)
+
+[Dynamic Expression From String](https://www.strathweb.com/2018/01/easy-way-to-create-a-c-lambda-expression-from-a-string-with-roslyn/)
+
